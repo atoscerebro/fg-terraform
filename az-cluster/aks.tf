ffd45599cbfd1b638db96dd3b8979785c6d89b45
Vidhyah Ganesharaja <vidhyah.ganesharaja@atos.net>
2023-02-01 12:16:40 +0000

    Added az-cluster subfolder and removed random name generator

---
+++ az-cluster/aks.tf
@@ -0,0 +1,40 @@
+resource "tls_private_key" "github_ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = "4096"
+}
+
+resource "azurerm_kubernetes_cluster" "k8s" {
+  # location            = azurerm_resource_group.rg.location
+  # name                = var.cluster_name
+  # resource_group_name = azurerm_resource_group.rg.name
+  # dns_prefix          = var.dns_prefix
+
+  # tags                = {
+  #   Environment = "Development"
+  # }
+
+  azure_active_directory_role_based_access_control {
+    managed                = true
+    admin_group_object_ids = var.aks_admin_group_object_ids
+    azure_rbac_enabled     = true
+  }
+  default_node_pool {
+    name       = "agentpool"
+    vm_size    = "Standard_D2_v2"
+    node_count = var.agent_count
+  }
+  linux_profile {
+    admin_username = "ubuntu"
+
+    ssh_key {
+      key_data = trimspace(tls_private_key.github_ssh_key.public_key_openssh)
+    }
+  }
+  network_profile {
+    network_plugin    = "kubenet"
+    load_balancer_sku = "standard"
+  }
+  identity {
+    type = "SystemAssigned"
+  }
+}
--- main.tf
+++ main.tf
@@ -1,45 +1,4 @@
-# Generate random resource group name
-resource "random_pet" "rg_name" {
-  prefix = var.resource_group_name_prefix
-}
-
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
-  name     = random_pet.rg_name.id
-}
-
-resource "azurerm_kubernetes_cluster" "k8s" {
-  location            = azurerm_resource_group.rg.location
-  name                = var.cluster_name
-  resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = var.dns_prefix
-  tags                = {
-    Environment = "Development"
-  }
   
-  default_node_pool {
-    name       = "agentpool"
-    vm_size    = "Standard_D2_v2"
-    node_count = var.agent_count
-  }
-  linux_profile {
-    admin_username = "ubuntu"
-
-    ssh_key {
-      key_data = trimspace(tls_private_key.github_ssh_key.public_key_openssh)
-    }
-  }
-  network_profile {
-    network_plugin    = "kubenet"
-    load_balancer_sku = "standard"
-  }
-  service_principal {
-    client_id     = var.aks_service_principal_app_id
-    client_secret = var.aks_service_principal_client_secret
-  }
-}
-
-resource "tls_private_key" "github_ssh_key" {
-  algorithm = "RSA"
-  rsa_bits  = "4096"
 }
