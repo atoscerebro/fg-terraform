9b4c66714a2caa2ae93900d689e9d78167f12872
Michiel Boekhoff <michielalexanderb@gmail.com>
2023-02-02 06:45:19 +0000

    Refactor AKS module

--- azure/aks/variables.tf
+++ azure/aks/variables.tf
@@ -1,22 +1,29 @@
 variable "agent_count" {
+  type    = number
   default = 3
 }
 
 # The following two variable declarations are placeholder references.
 # Set the values for these variable in terraform.tfvars
 variable "aks_service_principal_app_id" {
-  default = ""
+  type = string
 }
 
 variable "aks_service_principal_client_secret" {
-  default = ""
+  type      = string
+  sensitive = true
 }
 
 variable "cluster_name" {
-  default = "k8stest"
+  type = string
 }
 
 variable "dns_prefix" {
   default = "k8stest"
 }
 
+variable "aks_admin_group_object_ids" {
+  type        = list(string)
+  description = "The AAD object IDs to give administrative permissions to the cluster."
+  default     = []
+}
