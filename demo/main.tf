9ed4c5d1c4217d2f5bc1edd5ee6e6f644d60f6e6
Michiel Boekhoff <michielalexanderb@gmail.com>
2023-02-02 06:23:02 +0000

    Add demo code

---
+++ demo/main.tf
@@ -0,0 +1,29 @@
+locals {
+  resource_group_name = "demo-rg"
+  vnet_name           = "test-vnet"
+}
+
+module "vnet" {
+  source              = "../azure/vnet"
+  vnet_name           = local.vnet_name
+  resource_group_name = local.resource_group_name
+}
+
+module "vm" {
+  source                            = "../azure/virtual-machine"
+  name                              = "demo-vm"
+  resource_group_name               = local.resource_group_name
+  location                          = "westeurope"
+  storage_os_disk_name              = "osdisk"
+  storage_os_disk_managed_disk_type = "Standard_LRS"
+  admin_username                    = "atos"
+  subnet_name                       = "private"
+  virtual_network_name              = local.vnet_name
+  tags = {
+    AtosManaged : true
+  }
+
+  depends_on = [
+    module.vnet
+  ]
+}
