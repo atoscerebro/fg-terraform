182d5373d4a88ac7acd1a04750a2e6b498b8ff78
Michiel Boekhoff <michielalexanderb@gmail.com>
2023-02-02 06:03:07 +0000

    Remove specific nsg rules
    These are already included by default.

--- azure/vnet/main.tf
+++ azure/vnet/main.tf
@@ -23,34 +23,6 @@ resource "azurerm_network_security_group" "private_nsg" {
   location            = data.azurerm_resource_group.resource_group.location
 }
 
-resource "azurerm_network_security_rule" "private_disallow_inbound_internet" {
-  resource_group_name         = data.azurerm_resource_group.resource_group.name
-  name                        = "Disallow inbound internet access"
-  direction                   = "Inbound"
-  access                      = "Deny"
-  protocol                    = "*"
-  network_security_group_name = azurerm_network_security_group.private_nsg.name
-  priority                    = 500
-  destination_port_range      = "*"
-  source_port_range           = "*"
-  source_address_prefix = "*"
-    destination_address_prefix  = "*"
-}
-
-resource "azurerm_network_security_rule" "private_allow_loadbalancers" {
-  resource_group_name         = data.azurerm_resource_group.resource_group.name
-  name                        = "Allow inbound internet access from ALB"
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "*"
-  source_address_prefix       = "AzureLoadBalancer"
-  network_security_group_name = azurerm_network_security_group.private_nsg.name
-  priority                    = 400
-  destination_port_range      = "*"
-  source_port_range           = "*"
-  destination_address_prefix  = "*"
-}
-
 resource "azurerm_subnet_network_security_group_association" "private_subnet_nsg_association" {
   subnet_id                 = azurerm_subnet.private_subnet.id
   network_security_group_id = azurerm_network_security_group.private_nsg.id
