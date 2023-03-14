fe55e364af24c51fa0a406b2c01ab2bbcb25b30e
Laura Herrera <laura.herrera@sqrootcomputing.co.uk>
2023-03-14 12:07:32 +0000

    we are creating one vpc

--- aws/vpc/outputs.tf
+++ aws/vpc/outputs.tf
@@ -1,56 +1,56 @@
 output "vpc_id" {
   description = "The ID of the VPC"
-  value       = try(aws_vpc.fg_vpc[0].id, "")
+  value       = try(aws_vpc.fg_vpc.id, "")
 }
 
 output "vpc_arn" {
   description = "The ARN of the VPC"
-  value       = try(aws_vpc.fg_vpc[0].arn, "")
+  value       = try(aws_vpc.fg_vpc.arn, "")
 }
 
 output "vpc_cidr_block" {
   description = "The CIDR block of the VPC"
-  value       = try(aws_vpc.fg_vpc[0].cidr_block, "")
+  value       = try(aws_vpc.fg_vpc.cidr_block, "")
 }
 
 output "default_security_group_id" {
   description = "The ID of the security group created by default on VPC creation"
-  value       = try(aws_vpc.fg_vpc[0].default_security_group_id, "")
+  value       = try(aws_vpc.fg_vpc.default_security_group_id, "")
 }
 
 output "default_network_acl_id" {
   description = "The ID of the default network ACL"
-  value       = try(aws_vpc.fg_vpc[0].default_network_acl_id, "")
+  value       = try(aws_vpc.fg_vpc.default_network_acl_id, "")
 }
 
 output "default_route_table_id" {
   description = "The ID of the default route table"
-  value       = try(aws_vpc.fg_vpc[0].default_route_table_id, "")
+  value       = try(aws_vpc.fg_vpc.default_route_table_id, "")
 }
 
 output "vpc_instance_tenancy" {
   description = "Tenancy of instances spin up within VPC"
-  value       = try(aws_vpc.fg_vpc[0].instance_tenancy, "")
+  value       = try(aws_vpc.fg_vpc.instance_tenancy, "")
 }
 
 output "vpc_enable_dns_support" {
   description = "Whether or not the VPC has DNS support"
-  value       = try(aws_vpc.fg_vpc[0].enable_dns_support, "")
+  value       = try(aws_vpc.fg_vpc.enable_dns_support, "")
 }
 
 output "vpc_enable_dns_hostnames" {
   description = "Whether or not the VPC has DNS hostname support"
-  value       = try(aws_vpc.fg_vpc[0].enable_dns_hostnames, "")
+  value       = try(aws_vpc.fg_vpc.enable_dns_hostnames, "")
 }
 
 output "vpc_main_route_table_id" {
   description = "The ID of the main route table associated with fg_vpc VPC"
-  value       = try(aws_vpc.fg_vpc[0].main_route_table_id, "")
+  value       = try(aws_vpc.fg_vpc.main_route_table_id, "")
 }
 
 output "vpc_owner_id" {
   description = "The ID of the AWS account that owns the VPC"
-  value       = try(aws_vpc.fg_vpc[0].owner_id, "")
+  value       = try(aws_vpc.fg_vpc.owner_id, "")
 }
 
 output "private_subnets" {
