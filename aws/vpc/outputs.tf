8684f0509af65b19c11f14786100ca0be695307f
Laura Herrera <laura.herrera@sqrootcomputing.co.uk>
2023-03-15 14:24:59 +0000

    AWS - VPC module (#12)
    * AWS: VPC module with no NATgw, Igw on demand
    
    * adding outputs to vpc
    
    * we are creating one vpc
    
    * wrong var names
    
    * associations dont have tags
    
    * we changed the az name
    
    * az is a list
    
    * format a string
    
    * dhcp options are lists
    
    * Needs the subnetID not cidr
    
    * adding names to route tables
    
    * if IGW is created, it's only one
    
    * Lintin
    
    * tyding up var types, docs etc
    
    * Adding version for provider
    
    * not adding NET_ACLs just yet
    
    * Output the tags for use on other modules

---
+++ aws/vpc/outputs.tf
@@ -0,0 +1,99 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = try(aws_vpc.fg_vpc.id, "")
+}
+
+output "vpc_arn" {
+  description = "The ARN of the VPC"
+  value       = try(aws_vpc.fg_vpc.arn, "")
+}
+
+output "vpc_cidr_block" {
+  description = "The CIDR block of the VPC"
+  value       = try(aws_vpc.fg_vpc.cidr_block, "")
+}
+
+output "vpc_tags" {
+  description = "Map with all the tags for the VPC"
+  value       = try(aws_vpc.fg_vpc.id, "")
+}
+
+output "default_security_group_id" {
+  description = "The ID of the security group created by default on VPC creation"
+  value       = try(aws_vpc.fg_vpc.default_security_group_id, "")
+}
+
+output "default_network_acl_id" {
+  description = "The ID of the default network ACL"
+  value       = try(aws_vpc.fg_vpc.default_network_acl_id, "")
+}
+
+output "default_route_table_id" {
+  description = "The ID of the default route table"
+  value       = try(aws_vpc.fg_vpc.default_route_table_id, "")
+}
+
+output "vpc_instance_tenancy" {
+  description = "Tenancy of instances spin up within VPC"
+  value       = try(aws_vpc.fg_vpc.instance_tenancy, "")
+}
+
+output "vpc_enable_dns_support" {
+  description = "Whether or not the VPC has DNS support"
+  value       = try(aws_vpc.fg_vpc.enable_dns_support, "")
+}
+
+output "vpc_enable_dns_hostnames" {
+  description = "Whether or not the VPC has DNS hostname support"
+  value       = try(aws_vpc.fg_vpc.enable_dns_hostnames, "")
+}
+
+output "vpc_main_route_table_id" {
+  description = "The ID of the main route table associated with fg_vpc VPC"
+  value       = try(aws_vpc.fg_vpc.main_route_table_id, "")
+}
+
+output "vpc_owner_id" {
+  description = "The ID of the AWS account that owns the VPC"
+  value       = try(aws_vpc.fg_vpc.owner_id, "")
+}
+
+output "private_subnets" {
+  description = "List of IDs of private subnets"
+  value       = aws_subnet.private[*].id
+}
+
+output "private_subnet_arns" {
+  description = "List of ARNs of private subnets"
+  value       = aws_subnet.private[*].arn
+}
+
+output "private_subnets_cidr_blocks" {
+  description = "List of cidr_blocks of private subnets"
+  value       = compact(aws_subnet.private[*].cidr_block)
+}
+
+output "public_subnets" {
+  description = "List of IDs of public subnets"
+  value       = aws_subnet.public[*].id
+}
+
+output "public_subnet_arns" {
+  description = "List of ARNs of public subnets"
+  value       = aws_subnet.public[*].arn
+}
+
+output "public_subnets_cidr_blocks" {
+  description = "List of cidr_blocks of public subnets"
+  value       = compact(aws_subnet.public[*].cidr_block)
+}
+
+output "public_route_table_ids" {
+  description = "List of IDs of public route tables"
+  value       = aws_route_table.public[*].id
+}
+
+output "private_route_table_ids" {
+  description = "List of IDs of private route tables"
+  value       = aws_route_table.private[*].id
+}
