a6297e160db053569a150a134ee4acd1b705c726
Laura Herrera <laura.herrera@sqrootcomputing.co.uk>
2023-03-17 11:48:29 +0000

    starti with visibilty

--- aws/waf-acl-alb/main.tf
+++ aws/waf-acl-alb/main.tf
@@ -68,14 +68,10 @@ resource "aws_wafv2_web_acl" "fg_web_acl_alb" {
     content {
       name = rule.value.name
       priority = rule.value.priority
-      action {
-        rule.value.action
-      }
-      statement {
-        rule.value.statement
-      }
       visibility_config {
-        rule.value.visibility_config
+        cloudwatch_metrics_enabled = false
+        sampled_requests_enabled   = false
+        metric_name                = rule.value.metric_name
       }
     }
   }
