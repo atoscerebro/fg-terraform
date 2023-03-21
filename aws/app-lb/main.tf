c5f920b031e55bf768d862461bf4866a1a7801c0
Patrick Merchant <patrickmerchant9@gmail.com>
2023-03-21 16:50:35 +0000

    remove unused aws caller identity

--- aws/app-lb/main.tf
+++ aws/app-lb/main.tf
@@ -65,8 +65,6 @@ resource "aws_s3_bucket_policy" "fg_alb_access_logs" {
 
 data "aws_elb_service_account" "main" {}
 
-data "aws_caller_identity" "current" {}
-
 data "aws_iam_policy_document" "allow_alb_write_to_bucket" {
   count = (var.enable_access_logging && (var.s3_bucket_id == "")) ? 1 : 0
 
