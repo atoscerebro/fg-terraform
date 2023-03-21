42189a1bb4a66e3c1ffaf4bd841a23bb5097ef76
Laura Herrera <laura.herrera@sqrootcomputing.co.uk>
2023-03-21 16:36:00 +0000

    AWS - Correct linter warnings (#19)
    * Required version required :)
    
    * tidy count reference
    
    * latest ver
    
    * duh

--- aws/vpc/main.tf
+++ aws/vpc/main.tf
@@ -64,7 +64,7 @@ resource "aws_route" "public" {
 resource "aws_route_table_association" "public" {
   count = var.availability_zones
 
-  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  subnet_id      = element(aws_subnet.public[*].id, count.index)
   route_table_id = aws_route_table.public.id
 }
 
@@ -99,8 +99,8 @@ resource "aws_route_table" "private" {
 resource "aws_route_table_association" "private" {
   count = var.availability_zones
 
-  subnet_id      = element(aws_subnet.private.*.id, count.index)
-  route_table_id = element(aws_route_table.private.*.id, count.index)
+  subnet_id      = element(aws_subnet.private[*].id, count.index)
+  route_table_id = element(aws_route_table.private[*].id, count.index)
 }
 
 resource "aws_vpc_dhcp_options" "default" {
